898ea6ad6ead4487a86bd8954aa26f2ac2464e2b
Leonel Gabino <leonelgabino515@gmail.com>
2025-06-04 12:45:42 -0600

    actualizacion de url

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -1,6 +1,8 @@
 import server from "./server";
 
 server.listen(3006, () => {
-    console.log('Server is running on port 3000');
+    console.log('Server is running on port 3006');
     console.log('http://localhost:3006');
 });
+
+
